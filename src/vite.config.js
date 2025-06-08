65af62d2853ba15d7ed55f1cfe6f57d721f5a5bf
Adeeb Abubacker <adeebabu655@gmail.com>
2025-06-08 08:49:52 +0530

    Deploy clean build

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/AdeebAbu.github.io/', // 👈 MUST match your GitHub repo name
+  base: '/walt/', // 👈 MUST match your GitHub repo name
   plugins: [react()],
 })
 //------
